df886ec190a8c36dced9e171682e3c28cb1307fa
yeveyn <yeveyn@gmail.com>
2021-09-12 23:12:48 +0900

    0912
    계속 틀리다길래 엄청 고민했는데,, 알고보니 입력 자료형은 고려를 안하고 연산해서 그런 거였다.. 🤣🤣🤣

---
+++ AREUM/9월/Solution_음양더하기.java
@@ -0,0 +1,16 @@
+package com.pro.algo;
+
+public class Solution_음양더하기 {
+	 public int solution(int[] absolutes, boolean[] signs) {
+	        
+	        int sum=0;
+	        
+	        for(int i=0; i<signs.length; i++){
+	            if(signs[i]) sum+=absolutes[i];
+	            else sum-=absolutes[i];
+	        }
+	        
+	        
+	        return sum;
+	    }
+}
