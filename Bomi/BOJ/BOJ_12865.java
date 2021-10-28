78474ba47cca26d7aaf0a7ace62a2e6fb01b6598
구보미 <kbm0652@naver.com>
2021-10-28 23:15:50 +0900

    211028
    냅 색 기억 재활..

--- Bomi/BOJ/BOJ_12865.java
+++ Bomi/BOJ/BOJ_12865.java
@@ -1,35 +1,34 @@
 import java.util.*;
+import java.io.*;
 
-public class BOJ_12865 {
+public class BOJ_12865 {//평범한 배낭
 
-	public static void main(String[] args)   {
-		int n,k;
-		int dp[][],w[],v[]; 
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		n = sc.nextInt();
-		k = sc.nextInt();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
 		
-		dp = new int[n+1][k+1];
-		
-		w =new int[n+1];
-		v = new int[n+1];
-		
-		for(int i=1;i<=n;i++) {			
-			w[i] = sc.nextInt();
-			v[i]= sc.nextInt();
-		
+		int weights[] = new int[N+1];
+		int values[] = new int[N+1];
+		for(int i=1;i<=N;i++) {
+			st = new StringTokenizer(br.readLine());
+			weights[i] = Integer.parseInt(st.nextToken());
+			values[i] = Integer.parseInt(st.nextToken());
 		}
 		
+		int dp[][] = new int[N+1][K+1];
 		
-		for(int i=1;i<=n;i++) 
-			for(int j=1;j<=k;j++) {
-				dp[i][j] = dp[i-1][j]; 
-				if(j - w[i]>=0) 
-					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-w[i]]+v[i]); 
+		for(int i=1;i<=N;i++) {
+			for(int j=1;j<=K;j++) {
+				if(weights[i]>j) 
+					dp[i][j] = dp[i-1][j];
+				else 
+					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-weights[i]]+values[i]);
 			}
-
-		System.out.println(dp[n][k]);
+		}
 		
+		System.out.println(dp[N][K]);
 	}
 }
